e3e6fc17ce620d147e4a12c9e0e6132a21bf2306
johanM <johancmurillorivera@gmail.com>
2025-09-29 15:55:46 -0500

    calss event

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -30,7 +30,21 @@ class Event:
     date_: date
     start_at: time
     end_at: time
+    reminders: list[Reminder] = field(default_factory=list)
+    id: str = field(default_factory=generate_unique_id)
 
+    def add_reminder(self, date_time: datetime,type: str = Reminder.EMAIL):
+        reminder = Reminder(date_time=date_time, type=type)
+        self.reminders.append(reminder)
+
+    def delete_reminder(self, reminder_index: int):
+        if 0 <= reminder_index < len(self.reminders):
+            del self.reminders[reminder_index]
+        else:
+            reminder_not_found_error()
+
+    def __str__(self):
+        return (f"ID: {self.id}\n Event title: {self.title}\n Description:{self.description}\n Start at: {self.start_at}\n Time: { self.start_at} - {self.end_at}")
 #TODO: Implement Day class here
 
 
